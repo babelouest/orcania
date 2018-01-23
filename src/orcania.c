809a26d5c2b08f55ef29ab1389cfeb150f91aaeb
Nicolas Mora <github@babelouest.org>
2018-01-23 11:54:37 -0500

    Silvio's feedbacks

--- src/orcania.c
+++ src/orcania.c
@@ -247,7 +247,7 @@ char * o_strrchr(const char * haystack, int c) {
   }
 }
 
-#if defined(__linux__) || defined(__GLIBC__)
+#if defined(__linux__) || defined(__GLIBC__) || defined(_WIN32)
 static char *strnstr(const char *haystack, const char *needle, size_t len) {
   int i;
   size_t needle_len;
@@ -261,6 +261,22 @@ static char *strnstr(const char *haystack, const char *needle, size_t len) {
       return (char *)haystack;
 
     haystack++;
+  }
+  return NULL;
+}
+#endif
+
+#ifdef _WIN32
+static char *strcasestr(const char *haystack, const char *needle) {
+  size_t n;
+  if (haystack == NULL || needle == NULL) {
+    return NULL;
+  }
+  n = o_strlen(needle);
+  while (*haystack) {
+    if (!strnicmp(haystack++, needle, n)) {
+      return (char *)(haystack-sizeof(char));
+    }
   }
   return NULL;
 }
