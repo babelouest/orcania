4dffa725b2c103379c2e023bfc91bf578cb610e3
Nicolas Mora <github@babelouest.org>
2020-03-12 19:45:04 -0400

    Fix indent

--- test/memory_test.c
+++ test/memory_test.c
@@ -59,5 +59,5 @@ int main(int argc, char *argv[])
   number_failed = srunner_ntests_failed(sr);
   srunner_free(sr);
   
-	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
