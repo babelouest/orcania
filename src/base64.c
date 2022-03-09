10ce0baab9305322833e2ae406cbf98c2ac6c80d
Nicolas Mora <github@babelouest.org>
2022-03-09 10:45:21 -0500

    Rework o_base64 functions

--- src/base64.c
+++ src/base64.c
@@ -1,7 +1,7 @@
 /*
  * Base64 encoding/decoding (RFC1341)
  * Copyright (c) 2005-2011, Jouni Malinen <j@w1.fi>
- * Copyright (c) 2017-2020, Nicolas Mora <mail@babelouest.org>
+ * Copyright (c) 2017-2022, Nicolas Mora <mail@babelouest.org>
  *
  * This software may be distributed under the terms of the BSD license.
  */
@@ -13,17 +13,12 @@
 static const unsigned char base64_table[65] =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-/**
- * o_base64_encode - Base64 encode
- * @src: Data to be encoded
- * @len: Length of the data to be encoded
- * @out: Pointer to output variable
- * @out_len: Pointer to output length variable
- * Returns: 1 on success, 0 on failure
- *
- * The nul terminator is not included in out_len.
- */
-int o_base64_encode(const unsigned char * src, size_t len, unsigned char * out, size_t * out_len) {
+static const unsigned char base64_url_table[65] =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+static size_t table_size = 65;
+
+static int _o_base64_encode_agnostic(const unsigned char * src, size_t len, unsigned char * out, size_t * out_len, const unsigned char * table, int right_pad) {
   unsigned char * pos;
   const unsigned char * end, * in;
   size_t olen;
@@ -43,10 +38,10 @@ int o_base64_encode(const unsigned char * src, size_t len, unsigned char * out, 
   *out_len = 0;
   while (end - in >= 3) {
     if (pos != NULL) {
-      *pos++ = base64_table[in[0] >> 2];
-      *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-      *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
-      *pos++ = base64_table[in[2] & 0x3f];
+      *pos++ = table[in[0] >> 2];
+      *pos++ = table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+      *pos++ = table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+      *pos++ = table[in[2] & 0x3f];
     }
     (*out_len) += 4;
     in += 3;
@@ -58,56 +53,57 @@ int o_base64_encode(const unsigned char * src, size_t len, unsigned char * out, 
 
   if (end - in) {
     if (pos != NULL) {
-      *pos++ = base64_table[in[0] >> 2];
+      *pos++ = table[in[0] >> 2];
     }
     (*out_len)++;
     if (end - in == 1) {
       if (pos != NULL) {
-        *pos++ = base64_table[(in[0] & 0x03) << 4];
-        *pos++ = '=';
+        *pos++ = table[(in[0] & 0x03) << 4];
+        if (right_pad) {
+          *pos++ = '=';
+        }
       }
-      (*out_len) += 2;
+      if (right_pad) {
+        (*out_len) += 2;
+      } else {
+        (*out_len)++;
+      }
     } else {
       if (pos != NULL) {
-        *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-        *pos++ = base64_table[(in[1] & 0x0f) << 2];
+        *pos++ = table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+        *pos++ = table[(in[1] & 0x0f) << 2];
       }
       (*out_len) += 2;
     }
-    if (pos != NULL) {
+    if (pos != NULL && right_pad) {
       *pos++ = '=';
     }
-    (*out_len)++;
+    if (right_pad) {
+      (*out_len)++;
+    }
   }
 
-  if (pos != NULL) {
-    *pos = '\0';
-  }
   return 1;
 }
 
-/**
- * o_base64_decode - Base64 decode
- * @src: Data to be decoded
- * @len: Length of the data to be decoded
- * @out: Pointer to output variable
- * @out_len: Pointer to output length variable
- * Returns: 1 on success, 0 on failure
- *
- * The nul terminator is not included in out_len.
- */
-int o_base64_decode(const unsigned char *src, size_t len, unsigned char * out, size_t * out_len) {
-  unsigned char dtable[256], *pos = out, block[4], tmp;
-  size_t i, count;
+static int o_base64_decode_agnostic(const unsigned char *src, size_t len, unsigned char * out, size_t * out_len, const unsigned char * table, int right_pad) {
+  unsigned char dtable[256], *pos = out, block[4] = {0}, tmp;
+  size_t i, count, unpad = 0;
   int pad = 0;
   
   if (src == NULL || !len || out_len == NULL) {
     return 0;
   }
 
+  if (len % 4 == 2) {
+    unpad = 2;
+  } else if (len % 4 == 3) {
+    unpad = 1;
+  }
+
   memset(dtable, 0x80, 256);
-  for (i = 0; i < sizeof(base64_table) - 1; i++) {
-    dtable[base64_table[i]] = (unsigned char) i;
+  for (i = 0; i < table_size - 1; i++) {
+    dtable[table[i]] = (unsigned char) i;
   }
   dtable['='] = 0;
 
@@ -118,14 +114,14 @@ int o_base64_decode(const unsigned char *src, size_t len, unsigned char * out, s
     }
   }
 
-  if (count == 0 || count % 4) {
+  if (right_pad && (count == 0 || count % 4)) {
     return 0;
   }
 
   count = 0;
   *out_len = 0;
   for (i = 0; i < len; i++) {
-    if (!o_strchr((const char *)base64_table, src[i]) && src[i] != '=' && src[i] != '\n' && src[i] != '\t' && src[i] != ' ') {
+    if (!o_strchr((const char *)table, src[i]) && src[i] != '=' && src[i] != '\n' && src[i] != '\t' && src[i] != ' ') {
       // character invalid
       return 0;
     }
@@ -139,6 +135,10 @@ int o_base64_decode(const unsigned char *src, size_t len, unsigned char * out, s
     }
     block[count] = tmp;
     count++;
+    if (i == len-1) {
+      count = 4;
+      (*out_len) -= unpad;
+    }
     if (count == 4) {
       if (pos != NULL) {
         *pos++ = (block[0] << 2) | (block[1] >> 4);
@@ -164,10 +164,39 @@ int o_base64_decode(const unsigned char *src, size_t len, unsigned char * out, s
         }
         break;
       }
+      memset(block, 0, 4);
     }
   }
 
   return 1;
+}
+
+/**
+ * o_base64_encode - Base64 encode
+ * @src: Data to be encoded
+ * @len: Length of the data to be encoded
+ * @out: Pointer to output variable
+ * @out_len: Pointer to output length variable
+ * Returns: 1 on success, 0 on failure
+ *
+ * The nul terminator is not included in out_len.
+ */
+int o_base64_encode(const unsigned char * src, size_t len, unsigned char * out, size_t * out_len) {
+  return _o_base64_encode_agnostic(src, len, out, out_len, base64_table, 1);
+}
+
+/**
+ * o_base64_decode - Base64 decode
+ * @src: Data to be decoded
+ * @len: Length of the data to be decoded
+ * @out: Pointer to output variable, you might add up to 2 bytes to out length than expected for safety
+ * @out_len: Pointer to output length variable
+ * Returns: 1 on success, 0 on failure
+ *
+ * The nul terminator is not included in out_len.
+ */
+int o_base64_decode(const unsigned char *src, size_t len, unsigned char * out, size_t * out_len) {
+  return o_base64_decode_agnostic(src, len, out, out_len, base64_table, 1);
 }
 
 /**
@@ -181,68 +210,21 @@ int o_base64_decode(const unsigned char *src, size_t len, unsigned char * out, s
  * The nul terminator is not included in out_len.
  */
 int o_base64url_encode(const unsigned char * src, size_t len, unsigned char * out, size_t * out_len) {
-  int res = 0;
-  size_t index;
-  double calc;
-  
-  if (out != NULL) {
-    res = o_base64_encode(src, len, out, out_len);
-    if (res) {
-      if (*out_len >= 2 && out[*out_len - 2] == '=') {
-        out[*out_len - 2] = '\0';
-        (*out_len) -= 2;
-      } else if (*out_len && out[*out_len - 1] == '=') {
-        out[*out_len - 1] = '\0';
-        (*out_len)--;
-      }
-      for (index = 0; index < *out_len; index++) {
-        if (out[index] == '+') {
-          out[index] = '-';
-        } else if (out[index] == '/') {
-          out[index] = '_';
-        }
-      }
-    }
-  } else if (len && out_len != NULL) {
-    calc = (4 * (double)len) / 3;
-    if (calc != floor(calc)) {
-      calc = floor(calc)+1;
-    }
-    *out_len = calc;
-    res = 1;
-  }
-  return res;
+  return _o_base64_encode_agnostic(src, len, out, out_len, base64_url_table, 0);
 }
 
 /**
  * o_base64url_decode - Base64 decode (url format)
  * @src: Data to be decoded
  * @len: Length of the data to be decoded
- * @out: Pointer to output variable
+ * @out: Pointer to output variable, you might add up to 2 bytes to out length than expected for safety
  * @out_len: Pointer to output length variable
  * Returns: 1 on success, 0 on failure
  *
  * The nul terminator is not included in out_len.
  */
 int o_base64url_decode(const unsigned char *src, size_t len, unsigned char * out, size_t * out_len) {
-  int res = 0;
-  size_t src_cpy_len;
-  unsigned char * src_cpy;
-  
-  if (src) {
-    src_cpy = o_malloc(len+3);
-    if (src_cpy != NULL) {
-      if (o_base64url_2_base64(src, len, src_cpy, &src_cpy_len)) {
-        res = o_base64_decode(src_cpy, src_cpy_len, out, out_len);
-      } else {
-        res = 1;
-      }
-      o_free(src_cpy);
-    } else {
-      res = 0;
-    }
-  }
-  return res;
+  return o_base64_decode_agnostic(src, len, out, out_len, base64_url_table, 0);
 }
 
 /**
@@ -260,7 +242,7 @@ int o_base64url_2_base64(const unsigned char *src, size_t len, unsigned char * o
   int res = 0;
   size_t index;
   
-  if (src) {
+  if (src != NULL && len && out != NULL && out_len != NULL) {
     memcpy(out, src, len);
     *out_len = len;
     for (index = 0; index < len; index++) {
